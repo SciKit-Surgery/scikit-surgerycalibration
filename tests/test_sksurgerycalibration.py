d3da4c0a821395fd16428293a7bcf8c059cf38e3
Stephen Thompson <s.thompson@ucl.ac.uk>
2020-04-21 14:22:26 +0100

    Issue #4 put tests into lint

--- tests/test_sksurgerycalibration.py
+++ tests/test_sksurgerycalibration.py
@@ -4,24 +4,27 @@
 
 from sksurgerycalibration.ui.sksurgerycalibration_demo import run_demo
 from sksurgerycalibration.algorithms import addition, multiplication
-import six
 
 # Pytest style
 
-def test_using_pytest_sksurgerycalibration():
-    x = 1
-    y = 2
+def test_sksurgerycalibration():
+    """Test that the demo works"""
+    var_x = 1
+    var_y = 2
     verbose = False
     multiply = False
 
     expected_answer = 3
-    assert run_demo(x, y, multiply, verbose) == expected_answer
+    assert run_demo(var_x, var_y, multiply, verbose) == expected_answer
+
 
 def test_addition():
+    """addition returns right value"""
 
     assert addition.add_two_numbers(1, 2) == 3
 
+
 def test_multiplication():
+    """multiplication returns right value"""
 
     assert multiplication.multiply_two_numbers(2, 2) == 4
-
