3674c99fd8bc4aecc8a135c2aff4aaa641b96a9e
Matt Clarkson <m.clarkson@ucl.ac.uk>
2020-04-26 06:51:47 +0100

    Issue #3: Tidy up.

--- tests/video/test_chessboard_calibration.py
+++ tests/video/test_chessboard_calibration.py
@@ -17,16 +17,19 @@ def test_chessboard_mono():
         image = cv2.imread(file)
         images.append(image)
 
+    # This illustrates that the PointDetector sub-class holds the knowledge of the model.
     chessboard_detector = \
-        pd.ChessboardPointDetector((14, 10), # (Width, height), number of internal corners
-                                   3,        # Actual square size in mm
-                                   (1, 1)    # Scale factors. Here, images are 1920x1080 so no scaling needed.
-                                   )         # If images were 1920x540, you'd pass in (1920, 540),
-                                             # and PointDetector base class would scale it up.
+        pd.ChessboardPointDetector((14, 10),    # (Width, height), number of internal corners
+                                   3,           # Actual square size in mm
+                                   (1, 1)       # Scale factors. Here, images are 1920x1080 so no scaling needed.
+                                   )            # If images were 1920x540, you'd pass in (1920, 540),
+                                                # and PointDetector base class would scale it up.
 
+    # Pass a PointDetector sub-class to the calibration driver.
     calibrator = \
         mc.MonoVideoCalibration(chessboard_detector, 140)
 
+    # Repeatedly grab data, until you have enough.
     for image in images:
         successful = calibrator.grab_data(image)
         assert successful > 0
@@ -39,11 +42,12 @@ def test_chessboard_mono():
     assert successful
     assert calibrator.get_number_of_views() == 9
 
+    # Then do calibration
     reproj_err, recon_err, params = calibrator.calibrate()
 
-    print(reproj_err)
-    print(recon_err)
-    print(params)
+    # Just for a regression test, checking reprojection error, and recon error.
+    assert (np.abs(reproj_err - 0.59164921) < 0.000001)
+    assert (np.abs(recon_err - 0.21561650) < 0.000001)
 
 
 
