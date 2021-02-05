b833957eb0ffa290ef6fb1a413d307f5458e30ea
Thomas Dowrick <tdowrick@gmail.com>
2021-02-05 10:38:37 +0000

    Update requirement for surgeryimage

--- setup.py
+++ setup.py
@@ -57,7 +57,7 @@ setup(
         'scipy',
         'opencv-contrib-python',
         'scikit-surgerycore',
-        'scikit-surgeryimage>=0.7.4',
+        'scikit-surgeryimage>=0.8.4',
         'scikit-surgeryopencvcpp',
     ],
 
